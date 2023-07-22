07d2386a6b8144bee9f951210642d2195b4e28ca
amrrmohamad <amr.r.mohamad55@gmail.com>
2023-07-22 09:40:23 -0700

    alx

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+*main - Entry point
+*Description: A c program that prints with put function
+*Return: Always 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
