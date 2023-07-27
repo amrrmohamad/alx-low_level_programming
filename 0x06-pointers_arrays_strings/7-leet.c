91efa1b5977ad9131e9fa6c160e03c540e8f07b6
amrrmohamad <amr.r.mohamad55@gmail.com>
2023-07-27 08:37:13 -0700

    alx

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*leet - is a function
+*@s: is an argument
+*Return: Always 0
+*/
+char *leet(char *s)
+{
+	int i, j;
+	char arr[10][10]= {{'a','A','e','E','o','O','t','T','l','L'}, {'4','4','3','3','0','0','7','7','1','1'}};
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+	for (j = 0; j < 10; j++)
+	{
+	if (arr[0][j] == *(s + i))
+	*(s + i) = arr[1][j];
+	}
+	}
+	return (s);
+}
