6abd4050120ea7b01ab466d79d4443854f62a3e3
amrrmohamad <amr.r.mohamad55@gmail.com>
2023-07-28 04:52:33 -0700

    alx

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - check rhe code
+ *
+ * return: always 0.
+ */
+int main(void)
+{
+	print_number(98)
+	_putcher('\n');
+	print_number(402)
+	_putcher('\n');
+	print_number(1024)
+	_putcher('\n');
+	print_number(0)
+	_putcher('\n');
+	print_number(-98)
+	_putcher('\n');
+	return (0);
+}
