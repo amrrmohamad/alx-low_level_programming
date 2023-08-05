123ded7e7668a79cafdd285f1929c4be208ec880
amrrmohamad <amr.r.mohamad55@gmail.com>
2023-08-04 19:59:01 -0700

    alx

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - is entry point
+*@argc: is an argument
+*@argv: is an argument too
+*Return: Always 0
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
