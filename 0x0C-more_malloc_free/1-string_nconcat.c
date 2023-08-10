6435d54099c845bc8666ece46a180c5c54520467
amrrmohamad <amr.r.mohamad55@gmail.com>
2023-08-10 06:30:17 -0700

    alx

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	while (n < len2 && i < (len1 + n))
 		s[i++] = s2[j++];
-while (n >= len2 && i < (len1 + len2))
+	while (n >= len2 && i < (len1 + len2))
 		s[i++] = s2[j++];
 
 	s[i] = '\0';
