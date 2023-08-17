5311835e7d3fae5f9a2a508cd2e584fdfe36455d
amrrmohamad <amr.r.mohamad55@gmail.com>
2023-08-17 08:10:48 -0700

    alx

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef MA
+#define MA
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
