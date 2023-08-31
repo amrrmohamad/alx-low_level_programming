f1607cdcb9c04fc8f279908ddfe3699424097c23
amrrmohamad <amr.r.mohamad55@gmail.com>
2023-08-31 08:34:16 -0700

    alx

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+/**
+ * flip_bits -
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+	current = exclusive >> i;
+	if (current & 1)
+	{
+	count++;
+	}
+	}
+
+	return(count);
+}
+
