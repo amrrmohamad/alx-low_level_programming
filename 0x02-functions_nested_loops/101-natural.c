dc90efa8a8c3969bb8ca61077a701d7cf4cd1d45
amrrmohamad <amr.r.mohamad55@gmail.com>
2025-07-30 07:01:00 +0300

    alx

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-int main() {
+int main(void)
+{
+    int i;
     int sum = 0;
 
-    for (int i = 1; i < 1024; i++) {
+    for (i = 1; i < 1024; i++) {
         if (i % 3 == 0 || i % 5 == 0) {
             sum += i;
         }
